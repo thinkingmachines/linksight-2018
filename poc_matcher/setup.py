c64588c9324159c421594b9dcd91dbcb4ad78949
Mark Steve Samson <marksteve@thinkingmachin.es>
2018-04-06 11:36:12 +0800

    Rename package

--- poc_matcher/setup.py
+++ poc_matcher/setup.py
@@ -3,7 +3,7 @@ try:
 except ImportError:
     from distutils.core import setup
 
-setup(name='ontrack',
+setup(name='poc_matcher',
       version='1.0',
       py_modules=['cosine_matcher', 'text_cleaners', 'string_searcher'],
       install_requires=['numpy', 'pandas', 'nltk', 'scipy==0.16.1', 'scikit-learn'])
