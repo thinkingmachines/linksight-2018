e636298dd3bc34e7db05c9984f2e0531ffdf1ce1
Mark Steve Samson <marksteve@thinkingmachin.es>
2018-07-04 18:14:12 +0800

    Implement check API endpoint

--- linksight/api/urls.py
+++ linksight/api/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('datasets/<uuid:id>/preview', views.dataset_preview),
     path('datasets/<uuid:id>/match', views.dataset_match),
     path('matches/<uuid:id>/items', views.match_items),
+    path('matches/<uuid:id>/check', views.match_check),
 ]
