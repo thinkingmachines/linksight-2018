7775cf5a773545345c28d47ad3c92c2abb314bdd
Mark Steve Samson <marksteve@thinkingmachin.es>
2018-07-08 14:21:17 +0800

    Move file requirements copy inside the upload widget

--- linksight/app/src/components/upload-widget.js
+++ linksight/app/src/components/upload-widget.js
@@ -4,4 +4,12 @@ import {FilePond} from 'react-filepond'
 export default styled(FilePond)`
   z-index: 0;
   font-family: Barlow, sans-serif;
+  .filepond--drop-label label {
+    font-size: 15px;
+    padding-top: 0;
+    padding-bottom: 5px;
+  }
+  .filepond--drop-label label .requirements {
+    font-size: 12px;
+  }
 `
