34c97f4a4e1f42cc25747bc392a16d51d6d03af0
Mark Steve Samson <marksteve@thinkingmachin.es>
2018-08-01 11:14:11 +0800

    Disable service worker

--- app/src/index.js
+++ app/src/index.js
@@ -2,12 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './app'
 import {BrowserRouter} from 'react-router-dom'
-import registerServiceWorker from './registerServiceWorker'
 
 ReactDOM.render(
   <BrowserRouter><App /></BrowserRouter>,
   document.getElementById('app-root')
 )
-registerServiceWorker()
 
 window.modalRoot = document.getElementById('modal-root')
