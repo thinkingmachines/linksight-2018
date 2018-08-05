3f556b2986e5d1361391def4f3eda83ad31eb8dc
Mark Steve Samson <marksteve@thinkingmachin.es>
2018-08-05 21:16:54 +0800

    Fix topbar styling

--- app/src/components/topbar.js
+++ app/src/components/topbar.js
@@ -6,16 +6,24 @@ import * as colors from '../colors'
 
 const Topbar = styled(({ className }) => (
   <div className={className}>
-    <span>
-      This is alpha software! How can we make this tool better?
-    </span> <a href='https://airtable.com/shr7b1eauaxFWw1et'>
+    This is alpha software! How can we make this tool better?
+    <a href='https://airtable.com/shr7b1eauaxFWw1et'>
       Share your feedback!
     </a>
   </div>
 ))`
   background-color: ${colors.monochrome[2]};
-  padding: 10px;
   text-align: center;
+  font-size: 12px;
+  height: 30px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  z-index: 1;
+  a {
+    margin-left: 5px;
+  }
 `
 
 export default Topbar
