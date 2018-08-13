cbf72a7b1a07cd14f26b47d982d2b1d8e0c82b38
flo <flo@thinkingmachin.es>
2018-08-13 15:19:05 +0800

    Delete datasets only older than 24 hours

--- linksight/api/management/commands/delete_datasets.py
+++ linksight/api/management/commands/delete_datasets.py
@@ -1,10 +1,17 @@
+from datetime import timedelta
+
 from django.core.management.base import BaseCommand
+from django.utils import timezone
 
 from linksight.api.models import Dataset
 
+THRESHOLD = 24
+
 
 class Command(BaseCommand):
-    help = 'Deletes the public datasets'
+    help = 'Deletes the public datasets that are older than 24 hours'
 
     def handle(self, *args, **options):
-        Dataset.objects.filter(is_public_dataset=True).delete()
+        time_threshold = timezone.now() - timedelta(hours=THRESHOLD)
+        Dataset.objects.filter(is_internal=False,
+                               created_at__lt=time_threshold).delete()
