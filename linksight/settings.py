e4daf7138b713408a53dff3fd7bc44e2f9fb2edb
Mark Steve Samson <marksteve@thinkingmachin.es>
2018-08-15 11:10:50 +0800

    Add missing silk middleware and fix static root path

--- linksight/settings.py
+++ linksight/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'silk.middleware.SilkyMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -130,7 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'app/build/static')
 
 # Media files (Uploads)
 
