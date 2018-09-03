3fc8865dd726d8b9f08ae2de9297e70cdcf3ab24
Mark Steve Samson <marksteve@thinkingmachin.es>
2018-09-03 15:12:05 +0800

    Split out linksight.api.views

--- linksight/api/urls.py
+++ linksight/api/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from linksight.api import views
 
 urlpatterns = [
-    path('users/<id>', views.user_detail),
-    path('datasets/', views.dataset_list),
-    path('datasets/<uuid:id>/preview', views.dataset_preview),
-    path('datasets/<uuid:id>/match', views.dataset_match),
-    path('matches/<uuid:id>/items', views.match_items),
-    path('matches/<uuid:id>/save-choices', views.match_save_choices),
-    path('matches/<uuid:id>/preview', views.match_preview),
+    path('users/<id>', views.user.user_detail),
+    path('datasets/', views.dataset.dataset_list),
+    path('datasets/<uuid:id>/preview', views.dataset.dataset_preview),
+    path('datasets/<uuid:id>/match', views.dataset.dataset_match),
+    path('matches/<uuid:id>/items', views.match.match_items),
+    path('matches/<uuid:id>/save-choices', views.match.match_save_choices),
+    path('matches/<uuid:id>/preview', views.match.match_preview),
 ]
---
+++ linksight/api/views/dataset.py
@@ -0,0 +1,40 @@
+from django.shortcuts import get_object_or_404
+from linksight.api.models import Dataset
+from linksight.api.serializers import (DatasetMatchSerializer,
+                                       DatasetPreviewSerializer,
+                                       DatasetSerializer)
+from rest_framework.decorators import api_view, parser_classes
+from rest_framework.parsers import JSONParser, MultiPartParser
+from rest_framework.response import Response
+from silk.profiling.profiler import silk_profile
+
+
+@api_view(['POST'])
+@parser_classes((MultiPartParser,))
+def dataset_list(request):
+    serializer = DatasetSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=201)
+    return Response(serializer.errors, status=400)
+
+
+@api_view(['GET'])
+def dataset_preview(request, id):
+    dataset = get_object_or_404(Dataset, pk=id)
+    context = {'rows_shown': request.query_params.get('rowsShown', 10)}
+    serializer = DatasetPreviewSerializer(dataset, context=context)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+@parser_classes((JSONParser,))
+@silk_profile(name='Match dataset')
+def dataset_match(request, id):
+    serializer = DatasetMatchSerializer(data=request.data)
+    if serializer.is_valid():
+        dataset = get_object_or_404(Dataset, pk=id)
+        serializer.save(dataset=dataset)
+        return Response(serializer.data, status=201)
+    return Response(serializer.errors, status=400)
+
