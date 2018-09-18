1e98972703f07f994584c2916780dc0738eb26ec
Bianca Silmaro <biancasilmaro@gmail.com>
2018-09-18 11:22:24 +0800

    Remove clean data item in steps

--- app/src/preview.js
+++ app/src/preview.js
@@ -179,7 +179,6 @@ class Preview extends React.Component {
                 Select the columns in your dataset that refer to locations for cleaning. If you need to standardize either barangays or municipalities/cities, please choose at least two columns (ex: barangay & municipality, municipality & province).
               </p>
             </li>
-            <li>Clean your data</li>
             <li>Check and export</li>
           </ol>
         </Sidebar>
