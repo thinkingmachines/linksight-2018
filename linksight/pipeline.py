0a002b43f8ad42a62926a6627b10d164303f24ff
flo <flo@thinkingmachin.es>
2018-09-23 10:52:40 +0800

    Initial commit of sheets-based auth

--- linksight/pipeline.py
+++ linksight/pipeline.py
@@ -1,7 +1,34 @@
+import json
 from django.shortcuts import redirect
-from linksight.settings import HOST
+from linksight.settings import HOST, APPROVED_EMAILS_SHEETS_ID
+
+from apiclient.discovery import build
+
+
+def get_sheets_service():
+    return build('sheets', 'v4')
+
+
+def get_sheets_client():
+    service = get_sheets_service()
+    return service.spreadsheets()
+
+
+def get_sheet_values(spreadsheet_id, sheet_range):
+    client = get_sheets_client()
+    values = client.values().get(
+        spreadsheetId=spreadsheet_id,
+        range=sheet_range).execute().get('values', [])
+    fields = values.pop(0)
+    for value in values:
+        yield dict(zip(fields, value))
 
 
 def auth_allowed(backend, details, response, *args, **kwargs):
-    if not backend.auth_allowed(response, details):
+    approved_emails = [json.dumps(row) for row in get_sheet_values(APPROVED_EMAILS_SHEETS_ID,
+                                                                   'Sheet1!A')]
+
+    email = details.get('email')
+
+    if email not in approved_emails:
         return redirect('{}/?login-error'.format(HOST))
--- linksight/settings.py
+++ linksight/settings.py
@@ -178,6 +178,7 @@ SILENCED_SYSTEM_CHECKS = [
 # Datasets
 PSGC_DATASET_ID = env('PSGC_DATASET_ID')
 POPULATION_DATASET_ID = env('POPULATION_DATASET_ID')
+APPROVED_EMAILS_SHEETS_ID = env('APPROVED_EMAILS_SHEETS_ID')
 
 # Raven
 RAVEN_CONFIG = {
@@ -224,7 +225,7 @@ SOCIAL_AUTH_PIPELINE = (
     'social_core.pipeline.social_auth.social_details',
     'social_core.pipeline.social_auth.social_uid',
     'linksight.pipeline.auth_allowed',
-    'social_core.pipeline.social_auth.auth_allowed',
+    # 'social_core.pipeline.social_auth.auth_allowed',
     'social_core.pipeline.user.get_username',
     'social_core.pipeline.social_auth.associate_by_email',
     'social_core.pipeline.user.create_user',
