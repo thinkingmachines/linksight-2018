cbaef314bbc59a401fa305645d8fb967918c910b
iman <iman@syvlabs.com>
2018-10-23 17:30:05 +0800

    fix dockerfile

--- deploy/install-zmq.sh
+++ deploy/install-zmq.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
-# libzmq (from https://github.com/zeromq/libzmq)
-echo "deb http://download.opensuse.org/repositories/network:/messaging:/zeromq:/release-stable/Debian_9.0/ ./" >> /etc/apt/sources.list
-wget https://download.opensuse.org/repositories/network:/messaging:/zeromq:/release-stable/Debian_9.0/Release.key -O- | sudo apt-key add
-apt-get -yy install libzmq3-dev
+# Install zmq
+#   - From https://software.opensuse.org/download.html?project=network%3Amessaging%3Azeromq%3Arelease-stable&package=libzmq3-dev
+wget -nv https://download.opensuse.org/repositories/network:messaging:zeromq:release-stable/Debian_9.0/Release.key -O Release.key
+apt-key add - < Release.key
+apt-get update
 
-# jzmq
+echo 'deb http://download.opensuse.org/repositories/network:/messaging:/zeromq:/release-stable/Debian_9.0/ /' > /etc/apt/sources.list.d/network:messaging:zeromq:release-stable.list
+apt-get update
+apt-get -yy  install libzmq3-dev
+
+
+# Install jzmq
 apt-get -yy install pkg-config libtool autoconf automake
 git clone https://github.com/zeromq/jzmq.git
 cd jzmq/jzmq-jni/
